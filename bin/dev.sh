b881970646da037d7c02e84e4d19dd2748aca2bb
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-04 08:58:09 +0530

    Use ms_base

--- bin/dev.sh
+++ bin/dev.sh
@@ -3,4 +3,5 @@ exec erl -sname ms_logger_dev \
          -pa ebin deps/*/ebin test \
          -config config/dev \
          -boot start_sasl \
-         -s ms_logger_app
+         -s ms_logger_app \
+         -setcookie secret
