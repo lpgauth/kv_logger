8e1ae649359fdb1bc460fb83d9769d510c22e3c9
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-09-05 19:12:30 +0530

    Fix tests

--- test/ms_logger_test.erl
+++ test/ms_logger_test.erl
@@ -1,30 +1,38 @@
 -module(ms_logger_test).
--include("../include/ms_logger.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--define(LOG_FILE, "../log/2015-09-03-09.log").
+-define(LOG_FILE, "../.eunit/log/2015-09-03-09.log").
 -define(TIMESTAMP, {1441, 271599, 757400}).
 
 -spec test() -> ok.
 
-% -spec logger_test() -> ok.
-%
-% logger_test() ->
-%     % no_tty(),
-%
-%     ms_logger_app:start(),
-%
-%     file:delete(?LOG_FILE),
-%
-%     ms_logger:log(?TIMESTAMP, <<"test">>),
-%     ms_logger:log(?TIMESTAMP, <<"test">>),
-%
-%     {ok, Log} = file:read_file(?LOG_FILE),
-%     ?assertEqual(<<"">>, Log),
-%
-%     ms_logger_app:stop().
+%% runners
+-spec ms_logger_test_() -> ok.
 
-% no_tty() ->
-%     error_logger:tty(false),
-%     application:load(lager),
-%     application:set_env(lager, error_logger_redirect, false).
+ms_logger_test_() ->
+    {setup,
+        fun () -> setup() end,
+        fun (_) -> cleanup() end,
+        fun log/0
+    }.
+
+%% tests
+log() ->
+    ok = ms_logger:log(?TIMESTAMP, <<"test">>),
+    ok = ms_logger:log(?TIMESTAMP, <<"test">>),
+
+    {ok, Log} = file:read_file(?LOG_FILE),
+    ?assertEqual(<<"test\ntest\n">>, Log).
+
+%% utils
+cleanup() ->
+    ms_logger_app:stop().
+
+setup() ->
+    file:delete(?LOG_FILE),
+    error_logger:tty(false),
+    application:load(lager),
+    application:set_env(lager, error_logger_redirect, false),
+    application:load(fast_disk_log),
+    application:set_env(fast_disk_log, max_size, 1),
+    ms_logger_app:start().
